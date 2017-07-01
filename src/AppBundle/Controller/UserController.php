1e2f7a3ee87520faa91aa906fc64aeb77e6496eb
Eduardo Gómez <eduardogm81@gmail.com>
2017-07-01 16:52:43 +0200

    Añadida funcionalidad de editar los datos personales y la validación del formulario de registro.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Form\RegisterType;
+use AppBundle\Form\UserType;
 use BackendBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -123,6 +124,74 @@ class UserController extends Controller {
         }
 
         return new Response($result);
+    }
+
+    public function editUserAction(Request $request) {
+
+        // Recuperamos el usuario (que ya está logado) y generamos el formulario para editar los datos
+        $user = $this->getUser();
+        $user_image = $user->getImage();
+        $form = $this->createForm(UserType::class, $user);
+
+        // Recepción del formulario (hace un mapping con los valores del formulario y el objeto $user)
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+
+                // Cargamos el entity manager para poder usar la tabla User
+                $em = $this->getDoctrine()->getManager();
+
+                // Comprobamos si el usuario ya existe en la base de datos
+                $query = $em->createQuery("SELECT u FROM BackendBundle:User u
+                                          WHERE u.email = :email OR u.nick = :nick")
+                    ->setParameter("email", $form->get("email")->getData())
+                    ->setParameter("nick", $form->get("nick")->getData());
+                $user_isset = $query->getResult();
+
+                if (($user->getEmail() == $user_isset[0]->getEmail() && $user->getNick() == $user_isset[0]->getNick() ) || count($user_isset) == 0) {
+
+                    $file = $form["image"]->getData();
+
+                    if (!empty($file) && $file != null) {
+                        $ext = $file->guessExtension();
+                        if ($ext == "jpg" || $ext == "jpeg" ||$ext == "png" || $ext == "gif") {
+                            $file_name = $user->getId() . time() . "." . $ext;
+                            $file->move("uploads/users", $file_name);
+                            $user->setImage($file_name);
+                        } else {
+                            $user->setImage($user_image);
+                        }
+                    } else {
+                        $user->setImage($user_image);
+                    }
+
+                    $em->persist($user);
+                    $flush = $em->flush(); // Pasa los objetos que tenemos como persist a la base de datos
+
+                    if ($flush == null) {
+                        $status = "Se han modificado tus datos correctamente !!";
+                    } else {
+                        $status = "No se han modificado tus datos correctamente !!";
+                    }
+                } else {
+                    // Ya existe el usuario
+                    $status = "El usuario ya existe !!";
+                } // Si el usuario no existe
+
+            } else {
+                $status = "No se han actualizado tus datos!!";
+            } // si el formulario es válido (valid)
+
+            $this->session->getFlashBag()->add("status", $status);
+            return $this->redirect("my-data");
+
+        } // Si se ha recibido el formulario (submitted)
+
+
+        return $this->render("AppBundle:User:edit_user.html.twig", array(
+            "form" => $form->createView()
+        ));
+
     }
 
 }
